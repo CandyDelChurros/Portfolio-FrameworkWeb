09380166ab7b448ed92112e55b1aaa0f1b5a87d1
CandyDelChurros <cayo.veiga54@gmail.com>
2025-02-09 16:19:32 -0400

    lista 2 - completa e estilizada

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request, session,render_template_string, jsonify,flash, redirect, url_for
+from datetime import datetime
+import json
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+app.secret_key = "secret"
 
 @app.route("/")
 def home():
@@ -19,7 +23,6 @@ def l1e1():
 def l1e2():
     return render_template('l1e2.html')
 
-
 def generate_data():
     data = []
     for i in range(1, 998):
@@ -36,6 +39,131 @@ def generate_data():
 def l1e3():
     data = generate_data()
     return render_template('l1e3.html', data=data)
+
+USER = "snake"
+PASSWORD ="solidsnake"
+
+@app.route("/projetos/lista2/login", methods=["GET", "POST"])
+def login():
+    if "try" not in session:
+        session["try"] = 0
+
+    if request.method == "POST":
+        user = request.form["user"]
+        password = request.form["password"]
+
+        if session["try"] >= 2:
+            return render_template("login.html", msg="Você atingiu o limite de tentativas erradas. Tente novamente mais tarde.", block=True)
+
+        if user == USER and password == PASSWORD:
+            datenow = datetime.now().hour
+
+            if 5 <= datenow < 12:
+                hello = "Bom dia!"
+            elif 12 <= datenow < 18:
+                hello = "Boa tarde!"
+            else:
+                hello = "Boa noite!"
+                
+            session["try"] = 0
+            return render_template("welcome.html", hello=hello)
+        else:
+            session["try"] += 1
+            return render_template("login.html", msg="Login ou senha incorretos.", block=False)
+    
+    return render_template("login.html", msg=None, block=False)
+
+def template_json(json_data):
+    form_fields = json.loads(json_data)  
+    html_fields = ""
+    
+    for field in form_fields:
+        field_type = field['type']
+        field_name = field['name']
+        field_label = field['label']
+        
+        if field_type == 'text':
+            html_fields += f'<label for="{field_name}">{field_label}</label><input type="text" id="{field_name}" name="{field_name}"><br><br>\n'
+        elif field_type == 'number':
+            html_fields += f'<label for="{field_name}">{field_label}</label><input type="number" id="{field_name}" name="{field_name}"><br><br>\n'
+        elif field_type == 'email':
+            html_fields += f'<label for="{field_name}">{field_label}</label><input type="email" id="{field_name}" name="{field_name}"><br><br>\n'
+
+    template_html = f"""
+    <html>
+    <body>
+        <form method="POST" action="/projetos/l2e2/submit_form">
+            {html_fields}
+            <input type="submit" value="Enviar">
+        </form>
+        <a href="{{{{ url_for('portfolio') }}}}"><button>Voltar!</button></a>
+    </body>
+    </html>
+    """
+    return template_html
+
+@app.route('/projetos/l2e2')
+def forms():
+    json_data = json.dumps([
+        {"name": "nome", "label": "Nome", "type": "text"},
+        {"name": "idade", "label": "Idade", "type": "number"},
+        {"name": "email", "label": "E-mail", "type": "email"}
+    ])
+    
+    html_form = template_json(json_data)
+    return render_template_string(html_form)
+
+@app.route('/projetos/l2e2/submit_form', methods=['POST'])
+def submit_form():
+    dados = request.form.to_dict()
+    lista_strings = [f"{key}: {value}" for key, value in dados.items()]
+    return jsonify(lista_strings)
+
+users_db = {
+    "snake": generate_password_hash("solidsnake"),
+    "user2": generate_password_hash("mypassword"),
+}
+
+class User:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    @staticmethod
+    def authenticate(username, password):
+        if username in users_db and check_password_hash(users_db[username], password):
+            return User(username, password)
+        return None
+
+@app.route('/projetos/l2e3/login', methods=['GET', 'POST'])
+def l2e3():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user = User.authenticate(username, password)
+        if user:
+            session['username'] = user.username
+            flash('Login bem-sucedido!', 'success')
+            return redirect(url_for('dashboard'))
+        else:
+            flash('Usuário ou senha inválidos', 'danger')
+
+    return render_template('l2e3.html')
+
+
+@app.route('/projetos/l2e3/dashboard')
+def dashboard():
+    if 'username' in session:
+        return f'Bem-vindo ao seu painel, {session["username"]}!'
+    return redirect(url_for('l2e3'))
+
+
+@app.route('/projetos/l2e3/logout')
+def logout():
+    session.pop('username', None)
+    flash('Você saiu com sucesso', 'info')
+    return redirect(url_for('l2e3'))
 
 if __name__ == "__main__":
     app.run(debug=True)
